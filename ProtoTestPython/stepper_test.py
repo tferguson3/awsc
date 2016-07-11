2586f519a9bb1aa61b619c7bc1972faa37e460e5
greg8693 <gnmichaelson@alaska.edu>
2016-07-11 13:46:33 -0800

    updated to that on bob

--- ProtoTestPython/stepper_test.py
+++ ProtoTestPython/stepper_test.py
@@ -1,32 +1,37 @@
 #!/usr/bin/python
 import Adafruit_BBIO.GPIO as GPIO
 import time
-
 GPIO.setup("P8_9", GPIO.OUT) #step pin
-GPIO.setup("P8_11", GPIO.OUT) #direction pin
-
+GPIO.setup("P8_7", GPIO.OUT) #direction pin
+GPIO.setup("P8_11", GPIO.OUT) #ENABLE
 ##Define a function named Blink()
 def Blink(numTimes,speed):
+    GPIO.output("P8_11", GPIO.LOW)
     for i in range(0,numTimes):## Run loop numTimes
-        GPIO.output("P8_11", GPIO.HIGH)
+        GPIO.output("P8_7", GPIO.HIGH)
         print "Iteration " + str(i+1)## Print current loop
         GPIO.output("P8_9", GPIO.HIGH)
         time.sleep(speed)## Wait
         GPIO.output("P8_9", GPIO.LOW)
         time.sleep(speed)## Wait
     for i in range(0,numTimes):## Run loop numTimes
-        GPIO.output("P8_11", GPIO.LOW)
+        GPIO.output("P8_7", GPIO.LOW)
+        print "Iteration " + str(i+1)## Print current loop
+        GPIO.output("P8_9", GPIO.HIGH)
+        time.sleep(speed)## Wait
+        GPIO.output("P8_9", GPIO.LOW)
+        time.sleep(speed)## Wait
+    for i in range(0,numTimes):## Run loop numTimes
+        GPIO.output("P8_7", GPIO.LOW)
         print "Iteration " + str(i+1)## Print current loop
         GPIO.output("P8_9", GPIO.HIGH)
         time.sleep(speed)## Wait
         GPIO.output("P8_9", GPIO.LOW)
         time.sleep(speed)## Wait
     print "Done" ## When loop is complete, print "Done"
+    GPIO.output("P8_9", GPIO.LOW)
     GPIO.cleanup()
 
 ## Ask user for total number of blinks and length of each blink
 iterations = raw_input("Enter total number of times to step: ")
 speed = raw_input("Enter length of each step sequence (seconds): ")
-
-## Start Blink() function. Convert user input from strings to numeric data types and pass to Blink() as parameters
-Blink(int(iterations),float(speed))
