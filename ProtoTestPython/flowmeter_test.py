07a11e4d7ef7943f2ecc63bcf73d8ef472adda30
Aaron Dotson <addotson@alaska.edu>
2016-07-18 21:58:42 -0800

    updates

--- ProtoTestPython/flowmeter_test.py
+++ ProtoTestPython/flowmeter_test.py
@@ -2,19 +2,49 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time, sys
 
-GPIO.setup("P8_26", GPIO.IN)
+GPIO.setup("P9_12", GPIO.IN)
+GPIO.setup("P9_15", GPIO.IN)
+GPIO.setup("P9_23", GPIO.IN)
 
-global count
-count = 0
+global count1
+global count2
+global count3
 
-def countPulse(channel):
-   global count
-   count = count+1
-   print count
+count1 = 0
+count2 = 0
+count3 = 0
 
-GPIO.add_event_detect("P8_26", GPIO.RISING, callback=countPulse)
+def countPulse1(channel):
+   global count1
+   count1 = count1+1
+
+def countPulse2(channel):
+   global count2
+   count2 = count2+1
+
+def countPulse3(channel):
+  global count3
+  count3 = count3+1
+
+GPIO.add_event_detect("P9_12", GPIO.RISING, callback=countPulse1)
+GPIO.add_event_detect("P9_15", GPIO.RISING, callback=countPulse2)
+GPIO.add_event_detect("P9_23", GPIO.RISING, callback=countPulse3)
 
 while True:
+    start_counter = 1
+    count1=0
+    count2=0
+    count3=0
+    time.sleep(1)
+    start_counter = 0
+    flow1 = (count1 * 0.3256 + 5.2004)
+    flow2 = (count2 * 0.3256 + 5.2004)
+    flow3 = (count3 * 0.3256 + 5.2004)
+    print '\t%s\t%i\t%s\t%i\t%s\t%i' % ( "1: ",flow1,"2: ",flow2,"3: ",flow3)
+    time.sleep(1)
+    count1=0
+    count2=0
+    count3=0
     try:
         time.sleep(1)
     except KeyboardInterrupt:
