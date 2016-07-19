0e5f40cf92faef902f8b0b363b195a3f1ab10730
Aaron Dotson <addotson@uaa.alaska.edu>
2016-07-19 14:31:01 -0800

    revised with max flow restriction

--- ProtoTestPython/flowfreq.py
+++ ProtoTestPython/flowfreq.py
@@ -3,7 +3,8 @@ import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
 import time, sys
 
-#step pin on motor driver to be controlled by PWM below *note changed to P9_16 as not all listed PWM pins appear to function
+#step pin on motor driver to be controlled by PWM below *note changed to P9_16 as no
+t all listed PWM pins appear to function
 GPIO.setup("P8_7", GPIO.OUT) #direction pin
 GPIO.setup("P8_11", GPIO.OUT) #ENABLE motor driver
 GPIO.setup("P9_41", GPIO.IN) #flowmeter
@@ -15,14 +16,12 @@ global count
 global totalcount
 
 count = 0
-totalcount = 0
+totalflow = 0
 PWMstarted = 0
 
 def countPulse(channel):
    global count
-   global totalcount
    count = count+1
-   totalcount = totalcount+1
 
 GPIO.add_event_detect("P9_41", GPIO.RISING, callback=countPulse)
 
@@ -33,10 +32,12 @@ while True:
         time.sleep(1)
         start_counter = 0
         flow = count * 60.0 / 2200.0
-        stepf = flow * 0.02 * 1000 / 60 / 3 * 200 #must confirm mL per rotation as listed here with 3
-        totalflow = totalcount / 2200.0
-
-        print '%s%f\t%s%f\t%s%f' % ( "Flow (LPM): ",flow,"Step rate (Hz): ",stepf, "Total Flow (L): ", totalflow)
+	if flow >= 4.1: #set maximum flow to restrict miscalculation associated with electrical noise acceptable due to pd pump
+		flow = 4.1
+        stepf = flow * 0.02 * 1000 / 60 / 2.36 * 200
+        totalflow = totalflow + flow/60
+        print '%s%f\t%s%f\t%s%f' % ( "Flow (LPM): ",flow,"Step rate (Hz): ",stepf,"T
+otal Flow (L):",totalflow)
 
         if stepf >= 5 and PWMstarted == 0:
             GPIO.output("P8_11", GPIO.LOW)
