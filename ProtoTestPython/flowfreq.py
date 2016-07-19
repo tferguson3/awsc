8eadad57fbc6b4154becb9df01d18a29a7ed57c5
Aaron Dotson <addotson@alaska.edu>
2016-07-18 21:59:15 -0800

    added flow frequency code

---
+++ ProtoTestPython/flowfreq.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.PWM as PWM
+import time, sys
+
+#step pin on motor driver to be controlled by PWM below *note changed to P9_16 as not all listed PWM pins appear to function
+GPIO.setup("P8_7", GPIO.OUT) #direction pin
+GPIO.setup("P8_11", GPIO.OUT) #ENABLE motor driver
+GPIO.setup("P9_41", GPIO.IN) #flowmeter
+
+GPIO.output("P8_11", GPIO.HIGH)
+GPIO.output("P8_7", GPIO.LOW) #direction
+
+global count
+global totalcount
+
+count = 0
+totalcount = 0
+PWMstarted = 0
+
+def countPulse(channel):
+   global count
+   global totalcount
+   count = count+1
+   totalcount = totalcount+1
+
+GPIO.add_event_detect("P9_41", GPIO.RISING, callback=countPulse)
+
+while True:
+    try:
+        start_counter = 1
+        count=0
+        time.sleep(1)
+        start_counter = 0
+        flow = count * 60.0 / 2200.0
+        stepf = flow * 0.02 * 1000 / 60 / 3 * 200 #must confirm mL per rotation as listed here with 3
+        totalflow = totalcount / 2200.0
+
+        print '%s%f\t%s%f\t%s%f' % ( "Flow (LPM): ",flow,"Step rate (Hz): ",stepf, "Total Flow (L): ", totalflow)
+
+        if stepf >= 5 and PWMstarted == 0:
+            GPIO.output("P8_11", GPIO.LOW)
+            PWM.start("P9_16", 50, stepf, 1)
+            PWMstarted = 1
+
+        elif stepf >= 5 and PWMstarted == 1:
+            PWM.set_frequency("P9_16", stepf)
+
+        elif stepf < 5 and PWMstarted == 1:
+            PWM.stop("P9_16")
+            PWM.cleanup()
+            GPIO.output("P8_11", GPIO.HIGH)
+            PWMstarted = 0
+
+        #else:
+            #stepf < 5 and PWMstarted == 0 is do nothing
+    except KeyboardInterrupt:
+        print '\ncaught keyboard interrupt!, bye'
+        GPIO.cleanup()
+        sys.exit()
