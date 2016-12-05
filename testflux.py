a3d00c88bdf9dc7d4c80164db6e520730db0d131
greg8693 <gnmichaelson@alaska.edu>
2016-12-05 13:17:07 -0900

    testflux edit to avoid noise

--- testflux.py
+++ testflux.py
@@ -10,18 +10,18 @@ yesterday = date.today() - timedelta(days=1)
 year = yesterday.year
 month = yesterday.month
 day = yesterday.day
-#for day in range(1,2):
-print(day)
+#for day in range(1,7):
+#print(day)
 power1 = "flow"
 power2 = "pressure"
 
-# /home/addotson/data_AWSC/raw/
+## /home/addotson/data_AWSC/raw/
 
-#filename1 = "/home/addotson/data_AWSC/raw/{0}_{1}_{2}_conex-{3}".format(year, month, day, power1)
-#filename2 = "/home/addotson/data_AWSC/raw/{0}_{1}_{2}_conex-{3}".format(year, month, day, power2)
+filename1 = "/home/addotson/data_AWSC/raw/{0}_{1}_{2}_conex-{3}".format(year, $
+filename2 = "/home/addotson/data_AWSC/raw/{0}_{1}_{2}_conex-{3}".format(year, $
 
-filename1 = "/users/EnvEngineering/flux/{0}_{1}_{2}_conex-{3}".format(year, month, day, power1)
-filename2 = "/users/EnvEngineering/flux/{0}_{1}_{2}_conex-{3}".format(year, month, day, power2)
+#filename1 = "/users/EnvEngineering/flux/{0}_{1}_{2}_conex-{3}".format(year, m$
+#filename2 = "/users/EnvEngineering/flux/{0}_{1}_{2}_conex-{3}".format(year, m$
 i=1
 csv_reader1 = csv.reader(open(filename1+".csv"))
 next(csv_reader1) #ignore headers
@@ -33,12 +33,15 @@ gwrf=[0 for j in range(i)]
 intrf=[0 for j in range(i)]
 wwpf=[0 for j in range(i)]
 y=[0 for j in range(i)]
-dayroflux=0
+ddayroflux=0
 dayroflow=0
 dayropress=0
 daynfflux=0
 daynfflow=0
 daynfpress=0
+pf1=0
+pf2=0
+pf3=0
 j=0
 csv_reader1 = csv.reader(open(filename1+".csv"))
 next(csv_reader1)
@@ -50,7 +53,8 @@ for line in csv_reader1:
     wwpf[j]=(float(line[4]))
     y[j]=(line[0])
     j=j+1
-
+############## pressure reading
+#csv_reader1 = csv.reader(open(filename2+".csv"))
 ############## pressure reading
 #csv_reader1 = csv.reader(open(filename2+".csv"))
 i1=1
@@ -73,7 +77,7 @@ for line in csv_reader1:
     feed[j]=(float(line[1]))
     gwrp[j]=(float(line[2]))
     intrp[j]=(float(line[3]))
-    fltr2[j]=(float(line[4]))
+    fltr1[j]=(float(line[4]))
     fltr2[j]=(float(line[5]))
     y1[j]=(line[0])
     j=j+1
@@ -122,6 +126,7 @@ else:
 k=i1+30
 ROcount=0
 NFcount=0
+pfpcount=0
 filtervol=0
 ROflux=[0 for j in range(k)]
 NFflux=[0 for j in range(k)]
@@ -129,6 +134,9 @@ ROflow=[0 for j in range(k)]
 NFflow=[0 for j in range(k)]
 ROp=[0 for j in range(k)]
 NFp=[0 for j in range(k)]
+pfp=[0 for j in range(k)]
+pfp1=[0 for j in range(k)]
+pfp2=[0 for j in range(k)]
 dayroflux=1
 dayroflow=1
 daynfflux=1
@@ -162,18 +170,23 @@ for j in range(0,k):
         else: j=j
     elif y1[j+err1] == y[j+err]:
         #print("noterror")
-        if wwpf[j+err]>0 and intpf[j+err]==0:
+        if wwpf[j+err]>0.1 and intpf[j+err]<0.1:
             ROp[j]=(feed[j+err1]+intrp[j+err1])/2
             ROflux[j]=wwpf[j+err]/ROp[j]/0.0689476#equation for fluxgal/day/bar
             ROflow[j]=wwpf[j+err]/4
             ROcount=ROcount+1
-        elif intpf[j+err]>0 and wwpf[j+err]==0:
+        elif intpf[j+err]>0.1 and wwpf[j+err]<0.1:
             NFp[j]=(feed[j+err1]+gwrp[j+err1])/2
             NFflux[j]=intpf[j+err]/NFp[j]/0.0689476
             NFflow[j]=intpf[j+err]/4
             NFcount=NFcount+1
         if gwrf[j+err]!=0:#vol through prefilters
             filtervol=filtervol+gwrf[j+err]/4
+            if intpf[j+err]<0.1 and wwpf[j+err]<0.1:
+                pfp[j]=feed[j+err1]
+                pfp1[j]=fltr1[j+err1]
+                pfp2[j]=fltr2[j+err1]
+                pfpcount=pfpcount+1
 #factors are for L/M2 from gal/ft2
 dayroflux=sum(ROflux)/78/ROcount*1000#*3.79541/.0929
 dayroflow=sum(ROflow)
@@ -181,21 +194,23 @@ dayropress=sum(ROp)/ROcount
 daynfflux=sum(NFflux)/82/NFcount*1000#*3.79541/.0929
 daynfflow=sum(NFflow)
 daynfpress=sum(NFp)/NFcount
-
+pf1=sum(pfp)/pfpcount
+pf2=sum(pfp1)/pfpcount
+pf3=sum(pfp2)/pfpcount
 #print(NFcount)
 #print"Total Prefilter Volume= %f gal"%(filtervol)
 #print""
 #print"NF flow= %f gal" %(daynfflow)
 #print"NF ave.pressrue= %f psi"%(daynfpress)
-print"NF Flux= %f gal/day/ft2/bar"%(daynfflux)
+#print"NF Flux= %f gal/day/ft2/bar"%(daynfflux)
 #print""
 #print"RO flow= %f gal"%(dayroflow)
 #print"RO ave.pressrue= %f psi"%(dayropress)
-print"RO flux= %f gal/day/ft2/bar"%(dayroflux)
+#print"RO flux= %f gal/day/ft2/bar"%(dayroflux)
 
 
 ##writing to file next
 filename="flowpress_analysis.csv"
 file=open(filename,"a")
-file.write("\n%i%i%i,%f,%f,%f,%f,%f,%f,%f" % (year,month,day,filtervol,daynfflow,daynfpress,daynfflux,dayroflow,dayropress,dayroflux))
+file.write("%i_%i_%i,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n" % (year,month,day,filter$
 file.close()
