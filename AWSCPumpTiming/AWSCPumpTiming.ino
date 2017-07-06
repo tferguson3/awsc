fa8b42859a713cf40d1da16edf148255cca4e821
celubke <claire_lubke@hotmail.com>
2017-07-06 11:15:41 -0800

    Update AWSCPumpTiming.ino

--- AWSCPumpTiming/AWSCPumpTiming.ino
+++ AWSCPumpTiming/AWSCPumpTiming.ino
@@ -10,8 +10,8 @@ const int Air = 7;
 //Edit these times below to change factors
 unsigned long PumpInterval= 60000 *240;//How often in min
 unsigned long AirInterval= 60000 *15;//How often in min
-unsigned long PumpRunTime = 60000 *.3;//How Long in min
-unsigned long AirRunTime = 60000 *1;//How Long in min
+unsigned long PumpRunTime = 60000 *.4;//How Long in min
+unsigned long AirRunTime = 60000 *3;//How Long in min
 
 void setup() {
 pinMode(Pump, OUTPUT);
